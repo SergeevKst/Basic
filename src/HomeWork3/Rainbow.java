3602566bd709e8ae6f0ad23a1fbd38b3fa31dd98
Kst <andrewsergeev98@gmil.com>
2022-10-25 15:58:49 +0300

    homeworktask3

--- src/HomeWork3/Rainbow.java
+++ src/HomeWork3/Rainbow.java
@@ -8,61 +8,44 @@ public class Rainbow {
         int number1 = scan.nextInt();
         switch (number1) {
             case 1:
-                String str = "Red";
-                return str;
+                return "Red";
             case 2:
-                String str1 = "Orange";
-                return str1;
+                return "Orange" ;
             case 3:
-                String str2 = "Yellow";
-                return str2;
+                return "Yellow";
             case 4:
-                String str3 = "Green";
-                return str3;
+                return "Green";
             case 5:
-                String str4 = "Cyan";
-                return str4;
+                return "Cyan";
             case 6:
-                String str5 = "Blue";
-                return str5;
+                return "Blue";
             case 7:
-                String str6 = "Violet";
-                return str6;
+                return "Violet";
             default:
-                String str7="Number is not corrected, please enter corrected number";
-                return str7;
+                return "Number is not corrected, please enter corrected number";
         }
     }
 
     public String getTwoColor() {
         int number2 = scan.nextInt();
         if (number2 == 1) {
-            String str = "Red-" +getColor();
-            return str;
+            return "Red-" +getColor();
         } else if (number2 == 2) {
-            String str = "Orange-" + getColor();
-            return str;
+            return "Orange-" + getColor();
         } else if (number2 == 3) {
-            String str ="Yellow-"+ getColor() ;
-            return str;
+            return "Yellow-"+ getColor() ;
         } else if (number2 == 4) {
-            String str = "Green-"+getColor() ;
-            return str;
+            return "Green-"+getColor();
         } else if (number2 == 5) {
-            String str ="Cyan-"+ getColor() ;
-            return str;
+            return "Cyan-"+ getColor() ;
         } else if (number2 == 6) {
-            String str = "Blue-" +getColor();
-            return str;
+            return "Blue-" +getColor();
         } else if (number2 == 7) {
-            String str ="Violet-"+ getColor() ;
-            return str;
+            return "Violet-"+ getColor() ;
         } else if (number2 >= 8||number2<1) {
-            String str ="Number is not corrected, please enter corrected number";
-            return str;
+            return "Number is not corrected, please enter corrected number";
         }
-        String str = "";
-        return str;
+        return "";
     }
     public Rainbow(){
         System.out.println("If you want to know which color goes under which number, enter the number at 1 by 7 for example: 2 ");
