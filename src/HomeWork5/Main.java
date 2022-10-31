87738ac48710e20e95af40af5481ec763c9e5a4b
Kst <andrewsergeev98@gmil.com>
2022-11-01 00:42:53 +0300

    homeworktask5

--- src/HomeWork5/Main.java
+++ src/HomeWork5/Main.java
@@ -2,7 +2,8 @@ package HomeWork5;
 
 public class Main {
     public static void main(String[] args) {
-Matrix matrix=new Matrix();
-        System.out.println(matrix.creatDouble());
+        Matrix matrix = new Matrix();
+        matrix.toSplit();
+
     }
 }
